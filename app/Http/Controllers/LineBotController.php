cda42ede5cf7877fdc9a40149a049296e46f8533
katsukawagami <joelai1988@gmail.com>
2021-07-07 11:00:16 +0800

    add check sql data

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Stocks;
 use App\LineBot;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -50,7 +52,10 @@ class LineBotController extends Controller
     {
         switch ($request['events'][0]['message']['type']) {
             case 'text':
-                $message = 'text';
+                // 取得使用者對BOT輸入的文字
+                $inputText = $request['events'][0]['message']['text'];
+                // $message = 'text';
+                $message = $this->handleMessageText($inputText);
                 break;
 
             case 'sticker':
@@ -90,5 +95,72 @@ class LineBotController extends Controller
                 break;
         }
         return $message;
+    }
+
+    public function getStockPrice()
+    {
+        // Carbon::parse();
+        $dateStart = '2021-06-01';
+        $dateEnd = now();
+        $weekMap = [
+            0 => 'Sunday',
+            1 => 'Monday',
+            2 => 'Tuesday',
+            3 => 'Wednesday',
+            4 => 'Thursday',
+            5 => 'Friday',
+            6 => 'Saturday',
+            // 0 => '星期日',
+            // 1 => '星期一',
+            // 2 => '星期二',
+            // 3 => '星期三',
+            // 4 => '星期四',
+            // 5 => '星期五',
+            // 6 => '星期六',
+        ];
+        $timestampStart = strtotime($dateStart);
+        $timestampEnd = strtotime($dateEnd);
+        $targetStock = '0050';
+
+        $apiUrl = "https://query1.finance.yahoo.com/v8/finance/chart/{$targetStock}.TW?period1={$timestampStart}&period2={$timestampEnd}&interval=1d&events=history&=hP2rOschxO0";
+        $response = Http::get($apiUrl);
+
+        // // 應該是時間array
+        // $response['chart']['result'][0]['timestamp'];
+        // // 股價array
+        // $response['chart']['result'][0]['indicators']['quote'][0]['open'];
+        // $response['chart']['result'][0]['indicators']['quote'][0]['close'];
+
+        $data = [];
+        for ($i = 0; $i < count($response['chart']['result'][0]['timestamp']); $i++) {
+            $data[] = [
+                'timestamp' => Carbon::createFromTimestamp($response['chart']['result'][0]['timestamp'][$i], 'Asia/Taipei')->toDateString(),
+                'weekday' => $weekMap[Carbon::createFromTimestamp($response['chart']['result'][0]['timestamp'][$i], 'Asia/Taipei')->dayOfWeek],
+                'open' => number_format($response['chart']['result'][0]['indicators']['quote'][0]['open'][$i], 2),
+                'close' => number_format($response['chart']['result'][0]['indicators']['quote'][0]['close'][$i], 2),
+            ];
+        }
+
+        return $data;
+        // return $response['chart']['result'][0]['timestamp'];
+    }
+
+    public function handleMessageText($string)
+    {
+        // 在此METHOD處理文字訊息
+
+        // 轉小寫
+        $string = strtolower($string);
+        // 判斷該文字在stocks資料庫中存在與否
+        // !!!!!!!!!!!!!這個邏輯有同代碼多筆的問題存在!!!!待解決
+        // 目前先這樣測試功能
+        $stockData = Stocks::where('stock_code', '=', $string)->first();
+        if ($stockData !== null) {
+            $result = "購買價格 : {$stockData->purchase_price}\n購買日期 : {$stockData->purchase_date}\n數量 : {$stockData->amount}";
+        }else{
+            $result = '無資料';
+        }
+
+        return $result;
     }
 }
