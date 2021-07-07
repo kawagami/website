73bd51a553d12307253a3e698a95e02bf88d5aa8
katsukawagami <joelai1988@gmail.com>
2021-07-07 10:10:19 +0800

    arrange type of line bot input

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -17,19 +17,27 @@ class LineBotController extends Controller
     {
         if (count($request->events) > 0) {
             $replyToken = $request['events'][0]['replyToken'];
+
+            // 22-30行 的邏輯應該要分開寫，主要是依照進來訊息的不同做不同的回應
             // 這裡要判斷進來的是什麼類型的資料
-            $userMessage = $request['events'][0]['message']['text'] ?? '';
+            // $userMessage = $request['events'][0]['message']['text'] ?? '';
+            $userMessage = $this->handleMessageType($request);
+            // BOT要回覆的訊息
             $message = [
                 [
                     "type" => "text",
                     "text" => $userMessage
                 ]
             ];
+
+            // LINE的reply API
             $url = 'https://api.line.me/v2/bot/message/reply';
+            // LINE 固定的header 格式
             $header = [
                 'Content-Type' => 'application/json',
                 'Authorization' => 'Bearer ' . env('LINE_ACCESS_TOKEN'),
             ];
+            // 整理成ARRAY丟回去
             $data = [
                 'replyToken' => $replyToken,
                 'messages' => $message,
@@ -38,16 +46,49 @@ class LineBotController extends Controller
         }
         return response('success', 200);
     }
-    public function handleMessageType($eventArray)
+    public function handleMessageType($request)
     {
-        // $TextMessage;
-        // $StickerMessage;
-        // $ImageMessage;
-        // $VideoMessage;
-        // $AudioMessage;
-        // $LocationMessage;
-        // $ImagemapMessage;
-        // $TemplateMessage;
-        // $FlexMessage;
+        switch ($request['events'][0]['message']['type']) {
+            case 'text':
+                $message = 'text';
+                break;
+
+            case 'sticker':
+                $message = 'sticker';
+                break;
+
+            case 'image':
+                $message = 'image';
+                break;
+
+            case 'video':
+                $message = 'video';
+                break;
+
+            case 'audio':
+                $message = 'audio';
+                break;
+
+            case 'location':
+                $message = 'location';
+                break;
+
+            case 'imagemap':
+                $message = 'imagemap';
+                break;
+
+            case 'template':
+                $message = 'template';
+                break;
+
+            case 'flex':
+                $message = 'flex';
+                break;
+
+            default:
+                $message = '';
+                break;
+        }
+        return $message;
     }
 }
