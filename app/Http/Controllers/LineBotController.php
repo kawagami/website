a7d022ab007a7cc8d3dcdf962b59ee263fc7ac9d
katsukawagami <joelai1988@gmail.com>
2021-09-28 21:52:03 +0800

    add Line Message Validation Rule

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 // use App\Stocks;
 // use Carbon\Carbon;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Abstracts\Stocks as AbsStocks;
+use App\Http\Requests\LineBotRequest;
 
 class LineBotController extends AbsStocks
 {
@@ -15,14 +16,12 @@ class LineBotController extends AbsStocks
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index(LineBotRequest $request)
     {
-        if (count($request->events) > 0) {
-            Http::withHeaders($this->header)->post($this->url, [
-                'replyToken' => $this->replyToken,                    // 辨識用token
-                'messages'   => $this->handleMessageType($request),   // BOT要回覆的訊息
-            ]);
-        }
+        Http::withHeaders($this->header)->post($this->url, [
+            'replyToken' => $this->replyToken,                                 // 辨識用token
+            'messages'   => $this->handleMessageType($request->validated()),   // BOT要回覆的訊息
+        ]);
         return response('success', 200);
     }
 }
